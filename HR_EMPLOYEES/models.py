1b29939f3cf03cce5de16df52bdd645eb9763f01
unknown <sofa6099@gmail.com>
2023-02-02 13:33:24 +0200

    add date to leave

--- HR_EMPLOYEES/models.py
+++ HR_EMPLOYEES/models.py
@@ -170,6 +170,7 @@ class leave_request(models.Model):
     the_leave =  models.ForeignKey(DaysOff, on_delete=models.CASCADE , related_name="the_leave")
     number_of_days_requested = models.IntegerField(null=True)
     accepted = models.BooleanField(null=True)
+    started_at = models.DateField(auto_now=False, auto_now_add=False,null=True)
 
 
 
